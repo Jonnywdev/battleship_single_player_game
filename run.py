e263786783dc283ebf55d1574b2dc84b81327fb5
Jonnywdev <jonnywhitt19@gmail.com>
2023-03-13 11:34:12 +0000

    Added basic battleship game

--- run.py
+++ run.py
@@ -25,14 +25,14 @@ def create_battleships(game_board):
 
 
 def get_location_of_battleship():
-    board_row = input('Please enter a ship row 1-8')
+    board_row = input('Please enter a ship row 1-8: ')
     while board_row not in '12345678':
         print('Please enter a valid row')
-        board_row = input('Please enter a ship row 1-8')
-    board_column = input('Please enter a ship column A-H').upper()
+        board_row = input('Please enter a ship row 1-8: ')
+    board_column = input('Please enter a ship column A-H: ').upper()
     while board_column not in 'ABCDEFGH':
         print('Please enter a valid column')
-        board_column = input('Please enter a ship column A-H').upper()
+        board_column = input('Please enter a ship column A-H: ').upper()
     return int(board_row) - 1, letters_to_numbers[board_column]
 
 
@@ -46,7 +46,26 @@ def count_hit_battleships(game_board):
 
 
 create_battleships(HIDDEN_GAME_BOARD)
-turns = 10
 print_game_board(HIDDEN_GAME_BOARD)
-print_game_board(GUESS_GAME_BOARD)
-# while turns > 0:
+turns = 10
+while turns > 0:
+    print('Welcome to Battleship!')
+    print_game_board(GUESS_GAME_BOARD)
+    battleship_row, battleship_column = get_location_of_battleship()
+    if GUESS_GAME_BOARD[battleship_row][battleship_column] == '-':
+        print('Oops it looks like youve already guessed that!')
+    elif HIDDEN_GAME_BOARD[battleship_row][battleship_column] == 'X':
+        print('Woo! That is a HIT!')
+        GUESS_GAME_BOARD[battleship_row][battleship_column] = 'X'
+        turns -= 1
+    else:
+        print('Oh no! That was a miss.')
+        GUESS_GAME_BOARD[battleship_row][battleship_column] = '-'
+        turns -= 1
+    if count_hit_battleships(GUESS_GAME_BOARD) == 5:
+        print('Congratulations, you have sunk all of the computers battleships!')
+        break
+    print('you have ' + str(turns) + ' turns remaining.')
+    if turns == 0:
+        print('You ran out of turns! Game Over')
+        break
